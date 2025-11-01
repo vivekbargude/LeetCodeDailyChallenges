5734c7b13690c76aca2ccc6404fc8aa80ac7d63f
vivekbargude <vivekbargude1206@gmail.com>
2025-11-01 20:19:50 +0530

    1 November

---
+++ 3217. Delete Nodes From Linked List Present in Array.java
@@ -0,0 +1,97 @@
+// You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3], head = [1,2,3,4,5]
+
+// Output: [4,5]
+
+// Explanation:
+
+
+
+// Remove the nodes with values 1, 2, and 3.
+
+// Example 2:
+
+// Input: nums = [1], head = [1,2,1,2,1,2]
+
+// Output: [2,2,2]
+
+// Explanation:
+
+
+
+// Remove the nodes with value 1.
+
+// Example 3:
+
+// Input: nums = [5], head = [1,2,3,4]
+
+// Output: [1,2,3,4]
+
+// Explanation:
+
+
+
+// No node has value 5.
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+// All elements in nums are unique.
+// The number of nodes in the given list is in the range [1, 105].
+// 1 <= Node.val <= 105
+// The input is generated such that there is at least one node in the linked list that has a value not present in nums.
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums)
+        set.add(num);
+
+        if(head==null)
+        return head;
+
+        while(head!=null && set.contains(head.val))
+        head=head.next;
+
+        if(head == null || head.next == null )
+        return head;
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while(curr!=null){
+            while(curr!=null&&set.contains(curr.val)){
+                prev.next=curr.next;
+                curr=curr.next;
+            }
+
+            if(curr!=null)
+            curr=curr.next;
+
+
+            prev=prev.next;
+        }
+
+        return head;
+    }
+}
