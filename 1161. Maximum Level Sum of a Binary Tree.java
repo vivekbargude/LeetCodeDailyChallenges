d7ad8249304c635cc934b6897e72dc4277afdb15
vivekbargude <vivekbargude1206@gmail.com>
2026-01-06 18:47:31 +0530

    6 Jan 2026

---
+++ 1161. Maximum Level Sum of a Binary Tree.java
@@ -0,0 +1,79 @@
+// Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+// Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,7,0,7,-8,null,null]
+// Output: 2
+// Explanation: 
+// Level 1 sum = 1.
+// Level 2 sum = 7 + 0 = 7.
+// Level 3 sum = 7 + -8 = -1.
+// So we return the level with the maximum sum which is level 2.
+// Example 2:
+
+// Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+// Output: 2
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -105 <= Node.val <= 105
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public int maxLevelSum(TreeNode root) {
+//         Queue<TreeNode> queue = new LinkedList<>();
+
+//         queue.add(root);
+//         long level = 1;
+//         long maxSum = Long.MIN_VALUE;
+//         long maxLevel = 1;
+
+//         while(!queue.isEmpty()) {
+//             int size = queue.size();
+//             long currSum = 0;
+
+//             for(int i=0 ; i<size; i++) {
+//                 TreeNode node = queue.poll();
+//                 currSum+=node.val;
+
+//                 if(node.left!=null)
+//                 queue.add(node.left);
+
+//                 if(node.right!=null)
+//                 queue.add(node.right);
+//             }
+
+//             if(currSum > maxSum) {
+//                 maxSum = currSum;
+//                 maxLevel = level;
+//             }
+
+//             level++;
+
+//         }
+
+//         return (int) maxLevel;
+//     }
+// }
